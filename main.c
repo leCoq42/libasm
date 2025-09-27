0fa8b6355dfda8d0c69c380d9606d097d10111f6
Martin Haan <martin.haan@fox-it.com>
2025-09-27 12:13:47 +0200

    strlen testcase

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    test_strlen();
     return 0;
+}
+
+
+
+
+void test_strlen() {
+    char *str = "Hello, World!";
+    size_t len = ft_strlen(str);
+    printf("Length of '%s' is %zu\n", str, len);
 }
