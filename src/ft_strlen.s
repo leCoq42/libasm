fe1138385d32093b063e162b5af1e90f601fe141
Martin <martinhaan91+git@gmail.com>
2026-04-14 11:03:44 +0200

    added comment for .note.GNU-stack

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -18,3 +18,8 @@ ft_strlen:
 	ret ; return from function
 
 section .note.GNU-stack noalloc noexec nowrite progbits
+; .note.GNU-stack: special section name recognized by the GNU linker to configure stack execution policies.
+; noalloc: ensures the section is merely informational and not allocated in memory at runtime.
+; noexec: explicitly declares that the stack should not be granted execute permissions.
+; nowrite: indicates that the metadata section itself should not be writable.
+; progbits: specifies that the section contains explicit contents stored in the object file.
